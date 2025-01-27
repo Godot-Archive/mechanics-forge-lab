e09161ad446bd1ee496d6ea9922fca2ee8360e2a
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-01-27 23:34:57 +0530

    mouse based character rotation + projectile shooting mech

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+var bullet_path=(preload("res://scenes/bullet.tscn"))
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
@@ -7,6 +7,11 @@ const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
+	#Gun 
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("ui_accept"):
+		fire()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -41,3 +46,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func fire():
+	var bullet=bullet_path.instantiate()
+	bullet.dir=rotation
+	bullet.pos=$Node2D.global_position
+	bullet.rota=global_rotation
+	get_parent().add_child(bullet)
