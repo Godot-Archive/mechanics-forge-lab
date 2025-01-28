c21f08d29d126d817390d497211f2999dca3dbc4
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-01-28 19:45:06 +0530

    mouse controlled weapon implemented

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,35 +1,33 @@
 extends CharacterBody2D
-var bullet_path=(preload("res://scenes/bullet.tscn"))
+
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta: float) -> void:
-	#Gun 
-	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("ui_accept"):
-		fire()
-	
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Gets the input direction : -1, 0, 1
-	var direction := Input.get_axis("move_left", "move_right")
+	# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
 	
-	#Flip the sprite
+	# Flip the Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 	
-	# Play animation
+	# Play animations
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idol")
@@ -37,19 +35,11 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-		
 	
-	# Apply movement 
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
-func fire():
-	var bullet=bullet_path.instantiate()
-	bullet.dir=rotation
-	bullet.pos=$Node2D.global_position
-	bullet.rota=global_rotation
-	get_parent().add_child(bullet)
