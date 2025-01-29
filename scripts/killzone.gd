a49baec45e783a0e33077062e4228799f1673a94
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-01-29 13:30:19 +0530

    basic projectile shooter

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("Player dead!")
-	Engine.time_scale = 0.5
+	Engine.time_scale = 1.0
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
